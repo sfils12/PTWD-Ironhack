a9c9ea040b6250e65ea7caeffea878d4bfea17eb
Stanley Fils <stanleyfils@Stanleys-MacBook-Pro.local>
2019-12-09 20:58:29 -0500

    moved player code to player.js file

--- module-1-project/js/component.js
+++ module-1-project/js/component.js
@@ -1,28 +1,4 @@
-document.onkeydown = function (event) {
-  // console.log(event);
 
-  // use keycode.info to determine keystroke values
-  switch(event.keyCode){
-      case 37: // left
-      mainCharMarioX -= 15;
-      break;
-
-      case 39: // right
-      mainCharMarioX += 15;
-      break;
-
-      case 38: // up 
-      mainCharMarioY -= 15;
-      break;
-
-      case 40: // down
-      mainCharMarioY += 15;
-      break;
-
-      default:
-      console.log("I'm not sure what you're doing!");
-  }
-};
 
 
 
