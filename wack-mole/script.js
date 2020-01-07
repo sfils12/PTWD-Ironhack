99383d346c371c2a0aa0e58ca378f24bf1746e01
Stanley Fils <stanleyfils@Stanleys-MacBook-Pro.local>
2020-01-06 23:49:22 -0500

    Added Ironhack Kata folder

--- wack-mole/script.js
+++ wack-mole/script.js
@@ -1,36 +1,38 @@
-    console.log('hello');
+console.log('hello');
 
 window.onload = function(){
+
+ 
 
 
     let mole = document.createElement('div');
     mole.classList.add('mole');
 
-    //Code below is what Stevan helped me with in class. One line version of what Nick created below. 
-    //    document.getElementsByClassName('col')[Math.floor(Math.random() * document.getElementsByClassName('col').length)].appendChild(mole)
 
 
     function sendTheMole(){
-        let randomInt = Math.floor(Math.random() * 2000);
-        setInterval(()=>{
-            // In iteration 1, the mole simply appears in a different box once every second
+            mole.remove();
+           let randomNumWithGlobalScope = (Math.floor(Math.random() * 2000));
+
+           if(randomNumWithGlobalScope > 1500 || randomNumWithGlobalScope < 250){
+            setTimeout(sendTheMole, randomNumWithGlobalScope) 
+               return;
+           }
+            // right now the mole simply appears in a different box once every second
             // instead, let's make it so the mole sometimes stays for longer than a second
             // sometimes for less
             // and sometimes, dissappears and there is no mole for one or two seconds
 
-            //In iteration 2, the mole sometimes stays for longer than a second
-            // sometimes for less
-            // and sometimes, dissappears and there is no mole for one or two seconds
-
-            console.log(randomInt)
+            
             let boxes = document.getElementsByClassName('col');
             let randomBox = boxes[Math.floor(Math.random() * boxes.length)];
             
             randomBox.append(mole);
-            
-        }, randomInt)
+
+            setTimeout(sendTheMole, randomNumWithGlobalScope) 
     }
-    
+
+
 
 
 
@@ -44,7 +46,9 @@ window.onload = function(){
 
 
 
-
+mole.onclick = function(){
+    alert('you clicked the mole')
+}
 
 
 
